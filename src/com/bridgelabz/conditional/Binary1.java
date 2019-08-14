32aff62eb8e3ef2519d390165d88a9be25c77a49
Meher-D <dikshikameher22@gmail.com>
2019-08-14 13:40:56 +0530

    hii

---
+++ src/com/bridgelabz/conditional/Binary1.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.conditional;
+
+import java.util.Scanner;
+
+import com.bridgelabz.utility.ConditionalUtility;
+
+public class Binary1 {
+
+	public static void main(String[] args)
+	{
+		ConditionalUtility conditionalutility =new ConditionalUtility();
+		 Scanner scanner=new Scanner(System.in);
+		 conditionalutility.swapNibbles();
+		 
+	}
+
+}
